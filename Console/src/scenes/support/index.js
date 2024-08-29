934b0760595d0b1dd2747b90b94b01004fd13444
webamon <webamonuk@gmail.com>
2024-08-30 00:56:42 +0100

    fix: Changed oauth to apikey authentication & fixed login screen fields from hotfix/signup fix

--- Console/src/scenes/support/index.js
+++ Console/src/scenes/support/index.js
@@ -1,34 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box, Typography } from "@mui/material";
 import axios from "axios";
-import { Auth } from "aws-amplify";
+import { useAuth } from '../../AuthContext';
 
 const BugReport = () => {
     const [message, setMessage] = useState("");
     const [status, setStatus] = useState("");
-    const [email, setEmail] = useState("");
-
-    // Fetch the authenticated user's email on component mount
-    useEffect(() => {
-        const fetchUserEmail = async () => {
-            try {
-                const user = await Auth.currentAuthenticatedUser();
-                const email = user.attributes.email;
-                setEmail(email);
-            } catch (error) {
-                console.error("Error fetching user email:", error);
-            }
-        };
-
-        fetchUserEmail();
-    }, []);
+    const { apiKey } = useAuth();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         setStatus("Submitting...");
 
         try {
-            await axios.post("https://community.webamon.co.uk/bug", { message, email });
+            await axios.post(
+                "https://community.webamon.co.uk/bug",
+                { message }, // Body content
+                { // Configuration object
+                    headers: {
+                        'x-api-key': apiKey,
+                    },
+                }
+            );
             setStatus("Feedback submitted successfully!");
             setMessage(""); // Clear the message after submission
         } catch (error) {
@@ -48,12 +41,12 @@ const BugReport = () => {
                 padding: 2,
             }}
         >
-<Typography variant="h4" gutterBottom>
-    Please quickly tell us any feedback, bugs, features, anything!<br />
-    The whole reason you are here is to use the box below.<br />
-    Your feedback will directly influence actual output and solutions.<br />
-    Thank you!
-</Typography>
+            <Typography variant="h4" gutterBottom>
+                Please quickly tell us any feedback, bugs, features, anything!<br />
+                The whole reason you are here is to use the box below.<br />
+                Your feedback will directly influence actual output and solutions.<br />
+                Thank you!
+            </Typography>
             <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: 600, marginTop: "40px" }}>
                 <TextField
                     label="Message"
