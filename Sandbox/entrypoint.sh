6963c9fb1719ad486598b9544997c9132099a8b1
webamon <webamonuk@gmail.com>
2024-10-03 22:22:00 +0100

    ip tables config in entry point

--- Sandbox/entrypoint.sh
+++ Sandbox/entrypoint.sh
@@ -9,6 +9,13 @@ openvpn --config /etc/openvpn/nordvpn.ovpn --auth-user-pass /etc/openvpn/credent
 # Wait for the VPN connection to establish
 sleep 10
 
+# Set up iptables to allow traffic on port 5000 to bypass the VPN
+echo "Setting up iptables to allow LAN access to port 5000..."
+VPN_IFACE=$(ip route | grep default | awk '{print $5}')
+LOCAL_IFACE=$(ip route | grep -m1 -Eo 'dev [^ ]+' | awk '{print $2}')
+iptables -t nat -A POSTROUTING -o $VPN_IFACE -j MASQUERADE
+iptables -A FORWARD -i $LOCAL_IFACE -p tcp --dport 5000 -j ACCEPT
+
 # Run the main Python application
 echo "Starting the Webamon Sandbox application..."
 exec python webamon-sandbox.py
